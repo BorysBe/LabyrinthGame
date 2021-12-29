2926b72301a6970c5ba4d619f492aad9834570db
Robert Wiśniowski <robert.wisniowski@matrix-reliability.eu>
2021-12-29 11:42:10 +0100

    Kurczak: EnemySpawner added

--- Kurczak/Assets/Scripts/Terrain.cs
+++ Kurczak/Assets/Scripts/Terrain.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class Terrain : MonoBehaviour, ITerrain
 {
-    private BackgroundScroller _backgroundScroller;
+    //private BackgroundScroller _backgroundScroller;
+
+    public List<GameObject> _sprites = new List<GameObject>();
+    //public List<Material> _material = new List<Material>();
 
     public void Start()
     {
-        _backgroundScroller = GetComponent<BackgroundScroller>();
+        //_backgroundScroller = GetComponent<BackgroundScroller>();
+    }
+
+    public void Update()
+    {
+        
     }
 
     public void Move(float value)
     {
-        _backgroundScroller.OffsetUpdate(value);
+       // _backgroundScroller.OffsetUpdate(value);
     }
 
     public void MoveRight(float value)
