b6d0bb933a09d9e2bf836af7772b183a92197552
Robert Wiśniowski <robert.wisniowski@matrix-reliability.eu>
2021-12-29 16:01:52 +0100

    Kurczak: During damage dealer implementation.

---
+++ Kurczak/Assets/Scripts/ButtonTest.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ButtonTest : MonoBehaviour
+{
+    public void Test()
+    {
+        Debug.Log("I'm working");
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("I'm working");
+    }
+}
---
+++ Kurczak/Assets/Scripts/Enemy.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float health = 100;
+    [SerializeField] DamageDealer _damageDealer;
+    public float xposition;
+    public float yposition;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _damageDealer = GetComponent<DamageDealer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xposition = transform.position.x;
+        yposition = transform.position.y;
+        if (!Touchscreen.current.primaryTouch.press.isPressed)
+        {
+            return;
+        }
+
+        Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
+
+        Debug.Log(touchPosition);
+
+        if(touchPosition.x < transform.position.x + 100 && touchPosition.x > transform.position.x -100 && touchPosition.y < transform.position.y +100 && touchPosition.y > transform.position.y -100)
+        {
+            EnemyShooted();
+        }
+    }
+
+    public void EnemyShooted()
+    {
+
+        Debug.Log("enemy is shooted");
+        health -= _damageDealer.GetDamage();
+    }
+}
